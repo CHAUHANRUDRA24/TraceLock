4a9b83de2e595562d58677bfa984ee49aa7367b4
CHAUHANRUDRA24 <shani.rudra024@gmail.com>
2026-04-26 12:03:03 +0530

    Update Firebase config with Analytics and measurementId

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-analytics.js";
 import { getFirestore, collection, addDoc, serverTimestamp, query, orderBy, limit, onSnapshot, getCountFromServer, deleteDoc, doc, getDocs, where, setDoc, getDoc, updateDoc } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-firestore.js";
 import { getAuth, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
 import { getStorage, ref, uploadBytesResumable, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-storage.js";
@@ -9,11 +10,13 @@ const firebaseConfig = {
   projectId: "tracelock-2b9e3",
   storageBucket: "tracelock-2b9e3.firebasestorage.app",
   messagingSenderId: "893812064883",
-  appId: "1:893812064883:web:2c6b1a72ac718a4947e006"
+  appId: "1:893812064883:web:2c6b1a72ac718a4947e006",
+  measurementId: "G-SCBM6WB7W3"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 const storage = getStorage(app);
@@ -50,4 +53,4 @@ async function logAccess(documentId, traceId, event, node = "Global-Node-01") {
   }
 }
 
-export { db, auth, storage, logActivity, logAccess, collection, addDoc, serverTimestamp, query, orderBy, limit, onSnapshot, getCountFromServer, deleteDoc, doc, getDocs, where, setDoc, getDoc, updateDoc, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, ref, uploadBytesResumable, getDownloadURL, deleteObject };
+export { analytics, db, auth, storage, logActivity, logAccess, collection, addDoc, serverTimestamp, query, orderBy, limit, onSnapshot, getCountFromServer, deleteDoc, doc, getDocs, where, setDoc, getDoc, updateDoc, signOut, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, ref, uploadBytesResumable, getDownloadURL, deleteObject };
